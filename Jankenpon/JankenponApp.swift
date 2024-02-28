31834811a8837ef8511489f39d47fb64928df0f9
abgbro <codepath@abhigarlapati.com>
2024-02-28 17:29:31 -0500

    Initial Commit

---
+++ Jankenpon/JankenponApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenponApp.swift
+//  Jankenpon
+//
+//  Created by user256510 on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct JankenponApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
